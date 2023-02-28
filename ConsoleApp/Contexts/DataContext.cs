f366c6efd7af012a6cfb64df1a7ca2974bac4901
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2023-02-28 15:06:23 +0100

    Add project files.

---
+++ ConsoleApp/Contexts/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.Contexts
+{
+    //Denna klass har hand om kommunikationen mellan databasen och mina modeller
+    internal class DataContext : DbContext
+    {
+        private readonly string _connectionString = @""; 
+        public DataContext()
+        { 
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //Konfigurationen av databaskopplingen
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+
+    }
+}
